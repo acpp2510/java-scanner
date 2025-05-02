806b553abdfb262e577b5247e56cab1ca76d1d85
AnaCarinaPP <ancari1989@gmail.com>
2025-05-02 14:54:25 +0200

    add all features

--- SignUp.java
+++ SignUp.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class SignUp {
     public static void main(String[] args) {
         //Vamos a crear un formulario de registro
-
+        Scanner form = new Scanner(System.in);
         //Los datos que debes pedir al usuario son:
         //1. Nombre
         //2. Apellido
@@ -10,6 +12,24 @@ public class SignUp {
 
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
 
+        System.out.println("¿What´s your name?");
+        String name = form.nextLine();
+        System.out.println();
+
+        System.out.println("¿What´s your last name?");
+        String lastName = form.nextLine();
+        System.out.println();
+
+        System.out.println("¿What´s your user name?");
+        String userName = form.nextLine();
+        System.out.println();
+
+        System.out.println("¿What´s your password?");
+        String password = form.nextLine();
+
+        System.out.println();
+        form.close();
+        System.out.println("Hola "+ name + " "+ lastName+", tu nombre de usuario es " +userName+ " y tu contraseña es "+password+", gracias por registrarte.");
         //Imprimir el siguiente resultado:
         //Hola <nombre> <apellido>, tu nombre de usuario es <usuario> y tu contraseña es <contraseña>, gracias por registrarte.
         //Añade una nueva línea antes de mostrar la respuesta
